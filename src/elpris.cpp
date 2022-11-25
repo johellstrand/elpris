57447c2500a69e11fc9c7b67947b15718557b3c9
jhez <jan-olof.hellstrand@svenskaspel.se>
2022-11-25 07:36:38 +0100

    mo

--- src/elpris.cpp
+++ src/elpris.cpp
@@ -47,24 +47,23 @@ static int row;
 static int col;
 static int tableid;
 #include <map>
+#include <vector>
 #include <utility>
 
-static std::map<int, std::map<int,std::pair<std::string,std::string> > > tables;
+static std::map<int, std::map<int,std::vector<std::string> > > tables;
 
 static void find_definitions(GumboNode *node)
 {
     GumboAttribute *attr;
     if( col == 1 && node->type == GUMBO_NODE_TEXT )
     {
-        auto &[k,v] = tables[tableid-1][row-1];
-        k =  node->v.text.text;
+        tables[tableid-1][row-1].push_back(node->v.text.text );
         col++;
         return;
     }
     if( col == 2 && node->type == GUMBO_NODE_TEXT )
     {
-        auto &[k,v] = tables[tableid-1][row-1];
-        v =  node->v.text.text;
+        tables[tableid-1][row-1].push_back(node->v.text.text );
         col++;
         return;
     }
@@ -76,11 +75,17 @@ static void find_definitions(GumboNode *node)
     }
     
     if ( node->v.element.tag == GUMBO_TAG_TABLE ) {tableid++; row=col = 0; }
-
+    
+    if ( node->v.element.tag == GUMBO_TAG_TH )
+    {
+        row++; col = 0;
+        
+    }
+    
     if ( node->v.element.tag == GUMBO_TAG_TR ) {row++; col = 0; }
-
+    
     if ( node->v.element.tag == GUMBO_TAG_TD && col == 0) col = 1;
-
+    
     GumboVector *children = &node->v.element.children;
     for (int i = 0; i < children->length; ++i)
     {
@@ -90,7 +95,8 @@ static void find_definitions(GumboNode *node)
     return ;
 }
 
-static std::map<int, std::map<int,std::pair<std::string,std::string> > >  scrape(std::string markup)
+#include <fstream>
+static std::map<int, std::map<int,std::vector<std::string> > > scrape(std::string markup)
 {
     std::string res = "entry";
     GumboOutput *output = gumbo_parse_with_options(&kGumboDefaultOptions, markup.data(), markup.length());
@@ -103,20 +109,37 @@ static std::map<int, std::map<int,std::pair<std::string,std::string> > >  scrape
 }
 
 static const char* ht = "<table class=\"table table-striped mb30\"><thead><tr><th>Månad</th> <th>Månadspris (SE3)</th></tr></thead><tbody><tr><td>November 2022 *</td><td>85,57 öre/kWh</td></tr><tr><td>Oktober 2022</td><td>80,65 öre/kWh</td></tr><tr><td>September 2022</td><td>228,63 öre/kWh</td></tr><tr><td>Augusti 2022</td><td>223,05 öre/kWh</td></tr><tr><td>Juli 2022</td><td>86,61 öre/kWh</td></tr><tr><td>Juni 2022</td><td>126,31 öre/kWh</td></tr><tr><td>Maj 2022</td><td>102,86 öre/kWh</td></tr><tr><td>April 2022</td><td>89,22 öre/kWh</td></tr><tr><td>Mars 2022</td><td>130,33 öre/kWh</td></tr><tr><td>Februari 2022</td><td>77,48 öre/kWh</td></tr><tr><td>Januari 2022</td><td>104,33 öre/kWh</td></tr><tr><td>December 2021</td><td>180,74 öre/kWh</td></tr><tr><td>November 2021</td><td>83,52 öre/kWh</td></tr></tbody></table>";
+#include "ReadOnlyFileMMap.h"
 int main()
 {
-    // scrape( ht );
-    // exit(1);
-    auto html = request();
+    ReadOnlyFileMMap map("/tmp/elpris2.html");
+
+    std::string html;
+    if( map.open() )
+    {
+        html.assign( (const char*)map.const_data(), map.size() );
+    }
+    else
+    {
+        html = request();
+        std::ofstream myfile;
+         myfile.open ("/tmp/elpris2.html");
+         myfile << html;
+    }
+    
+    
     auto t = std::move(scrape(html));
-        
+    
     for( const auto& [a,b] : t )
     {
         for( const auto& [k,v] : b )
         {
-            const auto& [r,c] = v;
-            printf("%2d %-25.25s %-25.25s\n", a, r.c_str(), c.c_str() );
-//            std::cout << a << "-" << r << ":" << c << "\n";
+            printf("%2d ", a );
+            for( const auto& e : v )
+            {
+                printf("%-25.25s", e.c_str() );
+            }
+            puts("");
         }
     }
 }
